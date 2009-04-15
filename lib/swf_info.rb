3c46ae6f488548f25e44caf958a379dee363bccf
Matthew Ericson <mericson@ericson.net>
2009-04-15 17:11:14 -0400

    first

---
+++ lib/swf_info.rb
@@ -0,0 +1,103 @@
+  class SwfInfo 
+
+    require 'rubygems'
+    require 'bindata'
+    require 'open-uri'
+    require 'zlib'
+
+    attr_reader :path, :compressed, :version, :file_length, :width, :height
+    
+    def initialize( file ) 
+      
+      @path = file
+      
+      ##puts "Opening #{file}"
+      
+      open file do |f|
+        
+        sig = Sig.read( f )
+        ##puts "Compressed flag is #{sig.compressed_flag}"
+        ##puts "Version is #{sig.version}"
+        ##puts "File length is #{sig.file_length}"
+       
+        if sig.compressed_flag == 'C'
+          rest = CompressedData.read( f )
+          swf = Zlib::Inflate.inflate( rest.rest )
+          
+        else
+          rest = UncompressedData.read( f )
+          swf = rest.rest
+          
+        end
+        
+        ##i = 0
+        ##swf.each_byte do |b|
+          
+        ##  puts sprintf( "%02d: %08b %8d %2x %s", i, b, b, b, b.chr )
+        ##  break if i >= 20
+          
+        ##  i += 1
+        
+        ##end          
+        
+        size = PackedRect.read( swf )
+        rect = Rect.new( size.nbits, size.bit_array.collect { |q| q } )
+
+        ##puts "Rect legnth is #{size.nbits}"
+        ##puts "Size is #{a[0]/20}, #{a[2]/20} - #{a[1]/20}, #{a[3]/20}"          
+        
+        @path = file
+        @compressed = (sig.compressed_flag == 'C' )
+        @version = sig.version
+        @file_length = sig.file_length 
+
+        @width  = rect.values[1] / 20
+        @height = rect.values[3] / 20
+      end
+    end
+    
+    class SwfInfo::Sig < BinData::MultiValue
+      endian   :little
+      
+      string   :compressed_flag, :read_length => 1
+      string   :remainder_sig,   :read_length => 2
+      uint8    :version
+      uint32   :file_length
+    end
+
+    class SwfInfo::CompressedData < BinData::MultiValue
+      rest :rest
+    end
+    
+    class SwfInfo::UncompressedData < BinData::MultiValue
+      rest :rest
+    end
+    
+    class SwfInfo::PackedRect < BinData::MultiValue
+      bit5   :nbits
+      array  :bit_array, :type => :bit1, :initial_length => lambda { nbits * 4 }
+      array  :filler, :type => :bit1, :initial_length => lambda { 
+        skip_bits = ( nbits * 4 ) % 8 == 0 ? 0 : 8 - ( (nbits * 4) % 8 ) 
+      }
+      
+    end
+    
+    class SwfInfo::Rect 
+      
+      attr_reader :values
+
+      def initialize( nbits, data, field_map = {} )
+        @values = []
+        
+        ( data.length / nbits ).times do |i|
+          sum = 0
+          nbits.downto( 1 ) do |n|
+            exp = n - 1
+            sum += data.shift * (2 ** exp)
+          end
+          @values << sum
+        end
+      end
+    end
+  end
+
--- test/swf_info_test.rb
+++ test/swf_info_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
+require 'swf_info.rb' 
+
 class SwfInfoTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  context "Stadiums swf" do
+    setup do
+      @swf = SwfInfo.new( 'http://graphics8.nytimes.com/packages/flash/newsgraphics/2009/0402-ny-stadiums-hp/BasicProject.swf' )
+    end
+
+    should "be compressed" do
+      assert_equal true, @swf.compressed
+    end    
+    
+    should "be 337 pixels wide" do
+      assert_equal 337, @swf.width
+    end
+
+    should "be 290 pixels tall" do
+      assert_equal 290, @swf.height
+    end
+    
   end
+
+
 end
